b16bc4c786e6343c8a870363b3ae8bfb7a339104
Henri Umba <umbamw@gmail.com>
2018-09-08 23:26:15 -0400

    test: added a testRandomlyBind() for randomlyBind main: begin writting shuffle()
    testRandomlyBind() will help shuffle()

--- src/main/java/core/CardDeck.java
+++ src/main/java/core/CardDeck.java
@@ -1,5 +1,67 @@
 package core;
 
-public class CardDeck {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
 
+public class CardDeck {
+	
+	// Defining a Card
+	public class Card{
+		String rank;
+		char suit;
+		
+		public Card(String rank, char suit) {
+			this.rank = rank;
+			this.suit = suit;
+		}
+		
+		public String toString() {
+			return suit+rank;
+		}
+	}
+	
+	// Attributes / properties
+	private ArrayList<Card> deck;
+	private Random rand;
+
+	// Constructors
+	public CardDeck() {
+		deck = new ArrayList<>();
+		
+		// initialize 
+		char [] suits = {'H','C','D','S'};
+		String [] ranks = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+		for(String r: ranks)
+			for(char s: suits) {
+				deck.add(new Card(r, s));
+			}
+	}
+	
+	// getDeck()
+	public Collection<Card> getDeck(){
+		return deck;
+	}
+	
+	// shuffle()
+	public void shuffle() {
+		shuffle(deck.toArray());
+	}
+	
+	private <T> T[] shuffle(T[] subDeck) {
+		if(subDeck.length == 1)
+			return subDeck;
+		
+		T[] right = Arrays.copyOfRange(subDeck, 0, subDeck.length/2);
+		T[] left = Arrays.copyOfRange(subDeck, subDeck.length/2, subDeck.length);
+		
+		shuffle(right);
+		shuffle(left);
+		
+		
+		
+	}
+	
+	
 }
--- src/test/java/core/CardDeckTest.java
+++ src/test/java/core/CardDeckTest.java
@@ -2,44 +2,65 @@ package core;
 
 import java.util.Collection;
 
+import core.CardDeck.Card;
+
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 public class CardDeckTest extends TestCase{
-	
+
 	// Test getDeck
 	public void testGetDeck() {
 		CardDeck deck = new CardDeck();
-		
+
 		// Make sure it returns something
 		assertNotNull(deck.getDeck());
-		
+
 		// Make it turns an array (collections) type
-		assertEquals(Collection.class, deck.getDeck().getClass());
-		
+		assertEquals(52, deck.getDeck().size());
+
 		// Test if element type is CardDeck.Card
-		assertEquals(CardDeck.Card.class, deck.getDeck().get(0).getClass());
+		assertEquals(CardDeck.Card.class, ((ArrayList<CardDeck.Card>) deck.getDeck()).get(0).getClass());
 	}
-	
+
 	//Test shuffle
 	public void testShuffle() {
 		CardDeck deck = new CardDeck();
-		
+
 		Collection arr = deck.getDeck();
 		deck.shuffle();
-		
+
 		String temp1 = "";
 		String temp2 = "";
-		
+
 		for(CardDeck.Card e: deck.getDeck()) {
 			temp1 += e.toString();
 		}
-		
-		for(CardDeck.Card e: arr) {
+
+		for(Object e:arr) {
 			temp2 += e.toString();
 		}
-		
-	    assertEquals(false, temp1.compareTo(temp2)==0);
+
+		assertEquals(false, temp1.compareTo(temp2)==0);
 	}
-	
-	
+
+	public void testRandomlyBind() {
+		CardDeck deck = new CardDeck();
+		char [] arr1 = {'A','B','C','D','E','F','G','H','I','J','K','L','N'};
+		char [] arr2 = {'M','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+		char [] arr3 = deck.randomlyBind(arr1,arr2);
+
+		int idx = 0;
+
+		for(char x = 'A'; x <= 'Z'; x++) {
+			assertEquals(false, (arr1.toString().compareTo(arr2.toString())==0));
+		}
+
+
+
+	}
+
+
 }
